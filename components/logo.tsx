9eb6dd8ac51455c66f605e7959616da35e530cae
Henry <chunhyungjoon@gmail.com>
2024-06-08 17:34:36 +1000

    chore: Update logo and sideNav components

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,5 +1,20 @@
 import Link from 'next/link';
+import { buttonVariants } from './ui/button';
+import { SwitchCamera } from 'lucide-react';
 
 export default function Logo() {
-  return <Link href={'/dashboard'}>Logo</Link>;
+  return (
+    <Link
+      href={'/dashboard'}
+      className={buttonVariants({
+        className:
+          'hidden md:flex navLink !mb-10 lg:hover:bg-transparent lg:!p-0',
+        variant: 'ghost',
+        size: 'lg',
+      })}
+    >
+      <SwitchCamera className='h-6 w-6 shrink-0 lg:hidden' />
+      <p className={`font-semibold text-xl hidden lg:block `}>CooperGram</p>
+    </Link>
+  );
 }
--- components/navLinks.tsx
+++ components/navLinks.tsx
@@ -1,3 +1,78 @@
+'use client';
+
+import {
+  Clapperboard,
+  Compass,
+  Heart,
+  Home,
+  LinkIcon,
+  MessageCircle,
+  PlusSquare,
+  Search,
+} from 'lucide-react';
+
+import Link from 'next/link';
+import { buttonVariants } from './ui/button';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
+
+const links = [
+  { name: 'Home', href: '/dashboard', icon: Home },
+  {
+    name: 'Search',
+    href: '/dashboard/search',
+    icon: Search,
+    hideOnMobile: true,
+  },
+  { name: 'Explore', href: '/dashboard/explore', icon: Compass },
+  {
+    name: 'Reels',
+    href: '/dashboard/reels',
+    icon: Clapperboard,
+  },
+  {
+    name: 'Messages',
+    href: '/dashboard/messages',
+    icon: MessageCircle,
+  },
+  {
+    name: 'Notifications',
+    href: '/dashboard/notifications',
+    icon: Heart,
+    hideOnMobile: true,
+  },
+  {
+    name: 'Create',
+    href: '/dashboard/create',
+    icon: PlusSquare,
+  },
+];
+
 export default function NavLinks() {
-  return <div>NavLinks</div>;
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map(({ name, href, icon: Icon, hideOnMobile }) => {
+        const isActive = pathname === href;
+        <Link
+          key={name}
+          href={href}
+          className={buttonVariants({
+            variant: isActive ? 'secondary' : 'ghost',
+            className: cn('navLink', { 'hidden md:flex': hideOnMobile }),
+            size: 'lg',
+          })}
+        >
+          <Icon className='w-6' />
+          <p
+            className={`${cn('hidden lg:block', {
+              'font-extrabold': isActive,
+            })}`}
+          >
+            {name}
+          </p>
+        </Link>;
+      })}
+    </>
+  );
 }
